55bdaec83e851e64b96cd9417926e7d5e37ac120
knoyerlee <knoyerlee@gmail.com>
2021-12-29 23:09:44 -0500

    "Used black to format files"

--- src/create_df.py
+++ src/create_df.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+
 def create_df(data):
     df = pd.DataFrame(data)
-    custom_df = df[['name', 'price', 'day_high', 'day_low', '52_week_high', '52_week_low']]
+    custom_df = df[
+        ["name", "price", "day_high", "day_low", "52_week_high", "52_week_low"]
+    ]
     return custom_df
