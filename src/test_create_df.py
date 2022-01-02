04431ee7e6c81cd5cbf2fcd47065fb2b32a7ccf9
knoyerlee <knoyerlee@gmail.com>
2022-01-02 15:08:07 -0500

    added other files to tracking

---
+++ src/test_create_df.py
@@ -0,0 +1,50 @@
+import unittest
+import pandas as pd
+from create_df import create_df
+
+
+class TestCreateDf(unittest.TestCase):
+    def setUp(self):
+        self.stock_data = [
+            {
+                "ticker": "VOO",
+                "name": "Vanguard 500 Index Fund ETF",
+                "exchange_short": "NYSEARCA",
+                "exchange_long": "NYSE Arca",
+                "mic_code": "ARCX",
+                "currency": "USD",
+                "price": 436.4,
+                "day_high": 438.35,
+                "day_low": 436.41,
+                "day_open": 437.32,
+                "52_week_high": 440.36,
+                "52_week_low": 335.37,
+                "market_cap": None,
+                "previous_close_price": 437.79,
+                "previous_close_price_time": "2021-12-30T15:59:54.000000",
+                "day_change": -0.32,
+                "volume": 63168,
+                "is_extended_hours_price": False,
+                "last_trade_time": "2021-12-31T15:59:46.000000",
+            }
+        ]
+        self.stock_data_formatted = pd.DataFrame(
+            [
+                {
+                    "name": "Vanguard 500 Index Fund ETF",
+                    "price": 436.4,
+                    "day_high": 438.35,
+                    "day_low": 436.41,
+                    "52_week_high": 440.36,
+                    "52_week_low": 335.37,
+                }
+            ]
+        )
+
+    def test_create_df_positive(self):
+        created_data = create_df(self.stock_data)
+        self.assertEquals(str(self.stock_data_formatted), str(created_data))
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/test_stocks_api.py
@@ -0,0 +1,15 @@
+import unittest
+import unittest.mock
+from stocks_api import get_stocks
+
+
+class TestStocksApi(unittest.TestCase):
+    """Unit Tests for the stock api"""
+
+    def test_stocks_api_response(self):
+        """This test makes sure the info getting pulled down contains the expected values"""
+        self.assertIn("name", str(get_stocks("Voo")))
+
+
+if __name__ == "__main__":
+    unittest.main()
